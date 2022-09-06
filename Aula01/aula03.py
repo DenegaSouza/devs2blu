478925866c735934c7f87b92ac89e852028c43ae
Everton Denega Souza <everton.souza@proway.treina>
2022-09-06 19:52:44 -0300

    aula2

--- Aula01/aula03.py
+++ Aula01/aula03.py
@@ -8,3 +8,5 @@ print(f"O meu nome é {nome}, tenho {idade} anos e com um salário de R${salario
 #Opção de formatação com salario em 2 casas decimais
 print("O meu nome é {}, tenho {} anos e com um salário de R${:.2f}".format(nome, idade, salario))
 
+#Opção de formatação com interpolação e mostrando o tipo da variável
+print(f"O meu nome é {nome}, tenho {idade} anos e com um salário de R${salario:.2f}", type(nome), type(idade), type(salario))
