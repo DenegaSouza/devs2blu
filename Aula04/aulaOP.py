16f07ccf4bfaa6003c925cc5f25e5fd5e2c4b805
Everton Denega Souza <everton.souza@proway.treina>
2022-09-09 22:10:17 -0300

    aula4

---
+++ Aula04/aulaOP.py
@@ -0,0 +1,28 @@
+
+validador = True
+validador = False
+idade = int(input("Digite sua Idade: "))
+
+print(" "*5, "Expressão de igualdade", " "*5 )
+validador = ( idade == 18)
+print(validador)
+
+print(" "*5, "Expressão de diferença", " "*5 )
+validador = ( idade != 18)
+print(validador)
+
+print(" "*5, "Expressão de maior", " "*5 )
+validador = ( idade > 18)
+print(validador)
+
+print(" "*5, "Expressão de menor", " "*5 )
+validador = ( idade < 18)
+print(validador)
+
+print(" "*5, "Expressão de maior e igual", " "*5 )
+validador = ( idade >= 18)
+print(validador)
+
+print(" "*5, "Expressão de menor e igual", " "*5 )
+validador = ( idade <= 18)
+print(validador)
