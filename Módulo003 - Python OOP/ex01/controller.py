fff245104445cb8778a3a15750c2f990c73aee5b
Everton Denega Souza <evertonds@gmail.com>
2022-11-17 18:04:00 -0300

    ex01

---
+++ Módulo003 - Python OOP/ex01/controller.py
@@ -0,0 +1,39 @@
+from conta import Conta
+
+# Crie a  função create recebendo um atributo conta
+def create(conta):
+    # Crie uma variável contas escrevendo no arquivo txt
+    contas = open('contas.txt', 'a')
+    # Chame essa variável, chamando a função interna do python que escreve .write()
+    # Coloque o atributo da função dentro de parênteses convertido para string, 
+    # faça com que cada dado inserido seja escrito na próxima linha utilizando quebra de linha.
+    contas.write(str(conta)+'\n')
+    # Escreva a função  interna contas.close() para fechar o arquivo
+    contas.close()
+
+#Crie a função read  dentro do bloco da função.
+def read(conta):
+    # crie uma variável lista_contas recebendo uma lista vazia.
+    lista_contas = []
+    # Crie uma segunda variável contas abrindo nosso arquivo txt
+    contas = open('contas.txt', 'r')
+
+    # Crie um 'for' com uma variável conta percorrendo a variável contas do arquivo txt 
+    for conta in contas:
+        # faça novamente a variável do f'or conta' e atribua a função interna do python Que retira os espaços .strip
+        conta_objeto = conta.split(' ;')
+        # Crie uma variável conta_objeto recebendo a variável conta e 
+        # utilize a função interna do python que identifica um índice na lista .strip()
+        conta = conta.strip()
+
+        # Crie um objeto de conta e chame cada atributo da nossa classe titular número e saldo.
+        conta = Conta()
+        # insira para cada atributo um índice da lista
+        conta.titular = conta_objeto[0]
+        conta.numero = conta_objeto[1]
+        conta.saldo = conta_objeto[2]
+        # Chame a variável criada inicialmente lista_contas e recebendo a função interna do python .append(
+        lista_contas.append(conta)
+    # Chame a variável contas do arquivo txt e receba a função interna do python para fechar o arquivo txt .close()
+    contas.close()
+    return lista_contas
---
+++ Módulo003 - Python OOP/ex01/main.py
@@ -0,0 +1,27 @@
+# Crie um documento com namespace main e 
+# realize a importação da classe Conta e realize a importação, também do nosso CRUD e suas funções.
+
+from conta import Conta
+from controller import create, read
+
+# Crie uma funcao menu 
+def menu():
+    # Dentro do menu crie um objeto de conta
+    conta = Conta()
+    # Através da variável referência do nosso objeto chame cada atributo interno da nossa classe  e atribua valores aos mesmos
+    conta.titular = 'everton'
+    conta.numero = '123456'
+    conta.saldo = '5000'
+
+    # Chame a função create e salve os dados inseridos no nosso objeto no arquivo txt
+    create(conta)
+    # Crie uma variável lista_contas e atribua a função read
+    lista_contas = read(conta)
+    # Crie um print para imprimir lista_contas 
+    print(lista_contas)
+    # Crie um for com uma  c, variável  está, percorrendo a nossa variável que recebeu a função read lista_contas. 
+    # Dentro desse for crie um print imprimindo a variável c
+    for c in lista_contas:
+        print(c)
+
+menu()
